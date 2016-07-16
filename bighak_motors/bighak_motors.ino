f453eff5b5cdd61fdd2c3baef0925b4a6dbeb197
David <worlds6440@aol.com>
2016-07-16 11:15:23 +0100

    Added Arduino code for motors.

---
+++ bighak_motors/bighak_motors.ino
@@ -0,0 +1,196 @@
+#define SERIAL_BOARD_RATE 57600
+
+#define LEFT_MOTOR 0
+#define RIGHT_MOTOR 1
+#define LEFT_MOTOR_PIN A0
+#define RIGHT_MOTOR_PIN A1
+#define MOTOR_NEUTRAL 2.5
+#define MAX_PARAMS 3
+
+#define SPEED_MIN -1.0
+#define SPEED_MAX 1.0
+#define VOLT_MIN 0.0
+#define VOLT_MAX 5.0
+
+#define LED 13
+
+String m_szString;
+double m_dMotors_Current_V[2] = {MOTOR_NEUTRAL, MOTOR_NEUTRAL};
+double m_dMotors_Target_V[2] = {MOTOR_NEUTRAL, MOTOR_NEUTRAL};
+int m_nLastMilli = 0;
+double m_dVPerMilliSec = 0.0;
+
+void setup()
+{
+  // Configure USB serial
+  Serial.begin(SERIAL_BOARD_RATE, SERIAL_8N1);
+
+  // Setup LED flash pin
+  pinMode(LED, OUTPUT);
+
+  pinMode(LEFT_MOTOR_PIN, OUTPUT);
+  pinMode(RIGHT_MOTOR_PIN, OUTPUT);
+
+  // Calculate acceleration based on number of seconds from zero to full throttle.
+  calculate_acceleration(1.0);
+}
+
+void calculate_acceleration(float fSeconds)
+{
+  // Calculate acceleration based on number of seconds from zero to full throttle.
+  // [ratio of volts per second]
+  double dRangeV = (VOLT_MAX - VOLT_MIN) / 2.0; // Half range as want from zero speed to full in a set time.
+  m_dVPerMilliSec = dRangeV / (fSeconds*1000.0);
+}
+
+double speed_to_v(double dSpeed)
+{
+  // Convert range [-1,1] to [0,5]
+  double dV = MOTOR_NEUTRAL;
+  double dRangeSpeed = SPEED_MAX - SPEED_MIN;
+  double dRangeV = VOLT_MAX - VOLT_MIN;
+  double dDistanceSpeed = dSpeed - SPEED_MIN;
+  double dRatio = dDistanceSpeed / dRangeSpeed;
+  dV = 0.0 + (dRangeV * dRatio);
+  return dV;
+}
+
+void set_motor_target_speed(int nMotor, double dSpeed)
+{
+  // nMotor is index (L/R) and the speed is in range [-1,1]
+  m_dMotors_Target_V[nMotor] = speed_to_v(dSpeed);
+}
+
+void blink_LED(int nTimes)
+{
+  // Blink LED a set number of times.
+  for (int i=0; i<nTimes; i++)
+  {
+    digitalWrite(LED, HIGH);
+    delay(500);    
+    digitalWrite(LED, LOW);
+    delay(500);
+  }
+}
+
+void parse_command()
+{
+  // Parse the command just read in and enact on it.
+
+  // Parse command type from string
+  int nParam = 0;
+  String szParams[MAX_PARAMS];
+  bool bReading = false;
+  int nStrLength = m_szString.length();
+  for (int i=0; i<nStrLength; i++)
+  {
+    char chChar = m_szString[i];
+    if (chChar == '[')
+      bReading = true; // Found start of command string
+    else if (chChar == ',')
+      nParam++; // Comma means we move to next parameter index.
+    else if (chChar == ']' || chChar == '\0')
+      i=nStrLength; // Stop parsing string
+    else if (bReading)
+      szParams[nParam] += chChar;
+  }
+  // Convert found string to command index
+  int nCommand = 0;
+  if (szParams[0].length())
+    nCommand = szParams[0].toInt();
+  else
+    nCommand = -1; // If no command read, set to -1 so we know its not a valid command
+
+  // Blink LED to count command index
+  //blink_LED(nCommand);
+  
+  // Based on command type, read command parameters
+  switch(nCommand)
+  {
+    // CHANGE MOTOR SPEED
+    case 1:
+    {
+      // Blink when command recognised
+      //blink_LED(nParam+1);
+      
+      if (nParam == 2) // NOTE, nParam is zero based index so 2 == 3 parameters.
+      {
+        // Pull left and right motor speeds from command string.
+        float fLeftMotor = MOTOR_NEUTRAL, fRightMotor = MOTOR_NEUTRAL;
+        if (szParams[1].length()) // Only convert to float if string has a length.
+          fLeftMotor = szParams[1].toFloat();
+        if (szParams[2].length()) // Only convert to float if string has a length.
+          fRightMotor = szParams[2].toFloat();
+
+        // Set new TARGET speed. Note, doesn't actually set motor voltages here.
+        set_motor_target_speed(LEFT_MOTOR, fLeftMotor);
+        set_motor_target_speed(RIGHT_MOTOR, fRightMotor);
+      }
+    }break;
+  }
+}
+
+void read_command()
+{
+  // Read serial comms if available
+  bool bReadCMDEnd = false;
+  while (Serial.available()>0)
+  {
+    // Blink for each character read
+    //blink_LED(1);
+
+    char chChar = Serial.read();
+    m_szString += chChar;
+
+    if (chChar == ']')
+      bReadCMDEnd = true;
+  }
+  
+  // If we have read a command, attempt to parse out the details
+  if (bReadCMDEnd)
+  {
+    // Blink when end of command read
+    //blink_LED(1);
+    
+    parse_command();
+    m_szString = ""; // clear out string
+  }
+}
+
+void loop()
+{
+  // Read serial comms if available
+  read_command();
+
+  // First time ever into this loop.
+  // Ensure millis is time NOW.
+  int nMillis = millis();
+  if (m_nLastMilli == 0)
+    m_nLastMilli = nMillis;
+
+  // Loop motors [L/R] and update current voltage value based on acceleration ramps.
+  int nMotor = 0;
+  int nMotors = 2;
+  for (nMotor = 0; nMotor < nMotors; nMotor++)
+  {
+    // Calculate difference from current value and target value.
+    double dDiff = m_dMotors_Target_V[nMotor] - m_dMotors_Current_V[nMotor];
+    if (fabs(dDiff) > 0.001)
+    {
+      int nMilliDiff = nMillis - m_nLastMilli;
+      double dVIncrease = m_dVPerMilliSec * nMilliDiff;
+      m_dMotors_Current_V[nMotor] += (dDiff>=0.0? dVIncrease : -dVIncrease);
+      m_nLastMilli = nMillis;
+
+      // Send to Motors
+    }
+  }
+
+  // Turn on LED if we have reached target speed/voltage
+  if (fabs(m_dMotors_Current_V[LEFT_MOTOR]) >=4.5)
+    digitalWrite(LED, HIGH);
+  else
+    // Blink once each time round loop for status.
+    blink_LED(1);
+}
+
