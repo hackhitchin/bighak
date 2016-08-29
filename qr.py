b4f8fe0710decd062eb74b8650f6a1a838a2d3b7
David <worlds6440@aol.com>
2016-08-29 18:31:38 +0100

    QR mode upgraded

--- qr.py
+++ qr.py
@@ -20,7 +20,21 @@ class qr:
     def stop(self):
         """ Simple method to stop the RC loop """
         self.running = False
+        self.nCurrent_item = -1
         self.killed = True
+
+    def run(self):
+        """ Simple method to stop the RC loop """
+        self.running = True
+        self.nCurrent_item = -1
+        self.killed = False
+
+        route_length = len(self.route)
+        if route_length:
+            # Start Route by setting current item to zero
+            self.nCurrent_item = 0
+            # Calculate a finished time for each element to the route
+            self.calculate_finished_time()
 
     def command_seconds(self, command):
         """ return the number of seconds this command char takes. """
@@ -40,9 +54,11 @@ class qr:
         for index, x in enumerate(self.route):
             time_per_command = self.command_seconds(x[0])
             finish_time = (start_time +
-                           (time_per_command * x[1]) +
-                           self.nPause_between_commands)
+                           (time_per_command * x[1]))
             self.route[index] = (x[0], x[1], finish_time)
+            # The start time for the NEXT item is the
+            # finish time for this item with a small gap
+            start_time = finish_time + self.nPause_between_commands
 
     def loop(self, joystick, buttons):
         """ Single loop to determin controller state and send to motors
@@ -51,12 +67,6 @@ class qr:
             return  # Don't bother doing anything
 
         route_length = len(self.route)
-        if (self.nCurrent_item == -1 and route_length):
-            # Start Route by setting current item to zero
-            self.nCurrent_item = 0
-            # Calculate a finished time for each element to the route
-            self.calculate_finished_time()
-
         if self.current_item >= 0 and self.current_item < route_length:
             # Get current time and current item
             current_time = time.time()
@@ -66,6 +76,8 @@ class qr:
             if current_time > finish_time:
                 # Finished, skip to next item
                 self.current_item = self.current_item + 1
+                self.drive.set_neutral()
+                time.sleep(self.nPause_between_commands)
             else:
                 # Current item is the one we are working on right now.
                 if command == 'F':
@@ -76,6 +88,10 @@ class qr:
                     self.drive.mix_channels_and_send(0.0, -1.0, 0.0, 1.0)
                 elif command == 'R':
                     self.drive.mix_channels_and_send(0.0, 1.0, 0.0, -1.0)
+                elif command == 'P':
+                    print('P')  # Pause, don't need to do anything
+                elif command == 'Z':
+                    print('Z')  # Pulse laser on/off once.
         else:
             self.current_item = -1
 
